14f488cd77bb43453572b70d9ce4530b96351916
eva <lupmika@naver.com>
2022-05-17 16:58:22 +0900

    11장

--- contactsapp/src/main.js
+++ contactsapp/src/main.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import App from './AppAxiosTest.vue'
-import axios from 'axios'
+//import App from './AppAxiosTest.vue'
+//import axios from 'axios'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import ES6Promise from 'es6-promise'
 ES6Promise.polyfill()
 
+//Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
-Vue.prototype.$axios = axios
 
 new Vue({
-    render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
