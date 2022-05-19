66361d2b8edfd5bf9ffc3af8808e24fa15028c77
eva <lupmika@naver.com>
2022-05-19 15:19:21 +0900

    13장 연락처 예제에 트랜지션 적용

--- contactsapp/src/App.vue
+++ contactsapp/src/App.vue
@@ -9,7 +9,14 @@
            <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
          </div>
       </div>
-      <router-view></router-view>
+      <!-- 메인화면 전환시 플립 효과 일으키기 -->
+      <!-- transsition이 router-view를 감싸고 있으므로 router-view에 의해 나타나는 컴포넌트 변경시 트랜지션 효과가 적용된다.
+      mode: 트랜지션을 적용하는 순서, out-in 은 leave 단계 완료시 enter 단계가 시작한다는 의미
+      mode를 지정하지 않으면 in, out이 동시에 진행된다. in-out으로 지정시 enter 단계 완료후 leave 단계가 시작한다.
+      -->
+      <transition name="flip" mode="out-in">
+        <router-view></router-view>
+      </transition>
       <loading v-show="isloading"></loading>
   </div>
 </template>
@@ -33,4 +40,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.flip-enter-active{transition: all .4s ease-in-out;}
+.flip-leave-active{transition: all .4s ease-in-out;}
+.flip-enter, .flip-leave-to{transition: scaleY(0) translateZ(0); opacity: 0}
 </style>
--- contactsapp/src/components/ContactList.vue
+++ contactsapp/src/components/ContactList.vue
@@ -39,7 +39,9 @@
             :container-class="'pagination'"
             :page-class="'page-item'">
         </paginate>
-        <router-view></router-view>
+        <transition @before-enter="beforeEnter" @enter="enter" @leave="leave">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -47,6 +49,7 @@
 import Constant from '../Constant';
 import { mapState } from 'vuex';
 import Paginate from 'vuejs-paginate';
+import Velocity from 'velocity-animate';
 
 export default {
     name : 'contactList',
@@ -89,7 +92,57 @@ export default {
         },
         editPhoto : function(no) {
             this.$router.push({name:"updatephoto", params:{ no:no }});
-        }
+        },
+        beforeEnter:function(el){
+            el.style.opacity = 0
+        },
+        enter: function(el, done) {
+                Velocity(el, {
+                    opacity: 0,
+                    scale: 0.2
+                }, {
+                    duration: 200
+                })
+                Velocity(el, {
+                    opacity: 0.7,
+                    scale: 1.2
+                }, {
+                    duration: 200
+                })
+                Velocity(el, {
+                    opacity: 1,
+                    scale: 1
+                }, {
+                    complete: done
+                })
+            },
+            leave: function(el, done) {
+                Velocity(el, {
+                    translateX: '0px',
+                    opacity: 1
+                }, {
+                    duration: 100
+                })
+                Velocity(el, {
+                    translateX: '20px',
+                    opacity: 1
+                }, {
+                    duration: 100,
+                    loop: 2
+                })
+                Velocity(el, {
+                    translateX: '0px',
+                    opacity: 1
+                }, {
+                    duration: 200
+                })
+                Velocity(el, {
+                    translateX: '100px',
+                    opacity: 0
+                }, {
+                    complete: done
+                })
+            },
     }
 }
 </script>
